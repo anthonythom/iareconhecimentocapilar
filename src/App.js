4bce8fcab059495c0c4f573f282f4297e964e067
Anthony Thomas <95666367+anthonythom@users.noreply.github.com>
2023-11-10 09:51:25 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ImageClassification from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="App-wrapper">
+      
+        <ImageClassification />
+      </section>
+    </div>
+  );
+}
+
+export default App;
